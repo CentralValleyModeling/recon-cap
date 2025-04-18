26b145adcc146cfa0b5391770246339799a545cf
Raymond Hoang <raymond.hoang@water.ca.gov>
2025-04-18 10:15:07 -0700

    change url to recon-cap

--- run_server.sh
+++ run_server.sh
@@ -3,7 +3,7 @@
 # Activate environment
 source /env/bin/activate
 
-git_repo_url="https://github.com/CentralValleyModeling/general-recon.git"
+git_repo_url="https://github.com/CentralValleyModeling/recon-cap.git"
 
 # Clone the repo into a subfolder
 echo "cloning repo $1"
